646ee76cf50f5374d07651d4a66bf3512f5b2e65
qibaal-s <135332291+qibaal-s@users.noreply.github.com>
2025-04-15 19:10:16 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
-<?php
+<?php 
+use Illuminate\Support\Facades\Route; 
+use App\Http\Controllers\AnggotaController; 
 
-use Illuminate\Support\Facades\Route;
+Route::get('/', function () { 
+    return view('welcome'); 
+}); 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::resource('anggota', AnggotaController::class);
