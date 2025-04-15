ce9a6fb9ea38b21de938efe2a710bd0bab8bc742
qibaal-s <135332291+qibaal-s@users.noreply.github.com>
2025-04-15 20:05:17 +0700

    Delete routes/web.php

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php 
-use Illuminate\Support\Facades\Route; 
-use App\Http\Controllers\AnggotaController; 
-
-Route::get('/', function () { 
-    return view('welcome'); 
-}); 
-
-Route::resource('anggota', AnggotaController::class);
